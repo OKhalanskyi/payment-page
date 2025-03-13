31609ace6e0cf4e9fc3b41a1700b2ae6389341dd
OKhalanskyi <okhalanskyi@gmail.com>
2025-03-13 19:07:46 +0200

    fix: move footer to the bottom of the page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="text-center text-gray-600">
+    <footer className="text-center text-gray-600 mt-auto pt-12">
       {t('powered_by')}&nbsp;<strong>Solid</strong>
     </footer>
   );
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Layout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="bg-white p-4 lg:px-16 flex flex-col gap-4 text-gray-800">
+    <div className="bg-white p-4 lg:px-16 flex flex-col gap-4 text-gray-800 min-h-screen">
       {children}
     </div>
   );
